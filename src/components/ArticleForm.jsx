18f5f45c68c31e90ff0678bc477eae05887b92f3
최호찬 <starhochan70@gmail.com>
2026-06-25 09:47:28 +0900

    feat: CreateArticle/UpdateArticle 공통 폼 추출

---
+++ src/components/ArticleForm.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function ArticleForm({ initTitle = "", initDesc = "", initLevel = "", onSubmit }) {
+  const [content, setContent] = useState({
+    title: initTitle,
+    desc: initDesc,
+    level: initLevel,
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setContent(prev => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  return (
+    <form
+      action=""
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit(content.title, content.desc, content.level);
+      }}
+    >
+      <div>
+        <label htmlFor="title">title</label>
+        <input type="text" name="title" id="title" value={content.title} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="desc">desc</label>
+        <textarea name="desc" id="desc" value={content.desc} onChange={handleChange}></textarea>
+      </div>
+      <div>
+        <label htmlFor="level">level</label>
+        <input type="number" name="level" id="level" value={content.level} onChange={handleChange} />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+}
+export default ArticleForm;
--- src/components/createArticle.jsx
+++ src/components/createArticle.jsx
@@ -1,29 +1,11 @@
+import ArticleForm from "./ArticleForm";
+
 function CreateArticle({ onSubmit }) {
   console.log("CreateArticle render");
   return (
     <>
       <h2>Create Article</h2>
-      <form
-        action=""
-        onSubmit={e => {
-          e.preventDefault();
-          onSubmit(e.target.title.value, e.target.desc.value, e.target.level.value);
-        }}
-      >
-        <div>
-          <label htmlFor="title">title</label>
-          <input type="text" name="title" id="title" />
-        </div>
-        <div>
-          <label htmlFor="desc">desc</label>
-          <textarea name="desc" id="desc"></textarea>
-        </div>
-        <div>
-          <label htmlFor="level">level</label>
-          <input type="number" name="level" id="level" />
-        </div>
-        <button>Submit</button>
-      </form>
+      <ArticleForm onSubmit={onSubmit} />
     </>
   );
 }
